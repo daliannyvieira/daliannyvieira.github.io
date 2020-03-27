63e27bfdedd4ea0353cb3f546e1463548439c8e1
daliannyvieira <vieiradalianny@gmail.com>
2020-03-27 13:08:53 -0300

    clean styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -72,18 +72,27 @@ export default ({ data, pageContext }) => {
             justifyContent: `space-between`,
             listStyle: `none`,
             padding: 0,
+            margin: 0,
           }}
         >
           <li>
             {previous && (
-              <Link to={previous.fields.slug} rel="prev">
+              <Link
+                style={{ textDecoration: `none` }}
+                to={previous.fields.slug}
+                rel="prev"
+              >
                 ← {previous.frontmatter.title}
               </Link>
             )}
           </li>
           <li>
             {next && (
-              <Link to={next.fields.slug} rel="next">
+              <Link
+                style={{ textDecoration: `none` }}
+                to={next.fields.slug}
+                rel="next"
+              >
                 {next.frontmatter.title} →
               </Link>
             )}
