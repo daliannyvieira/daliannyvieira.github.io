718db2fbe8b88dc877728c748dd0ae9cfabaa1ca
daliannyvieira <vieiradalianny@gmail.com>
2020-04-08 19:19:26 -0300

    adding about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import image01 from "../images/me.jpg"
 import image03 from "../images/03.jpg"
-import image04 from "../images/04.jpeg"
+import image09 from "../images/09.jpeg"
 import image05 from "../images/05.jpg"
 import image06 from "../images/06.jpg"
 import image07 from "../images/07.jpg"
@@ -34,23 +34,36 @@ const AboutPage = ({ data }) => {
       />
 
       <div className="about-container">
+        <h1>Tech as a gift</h1>
         <div className="about-gallery">
-          <img className="image-about" src={image01} alt="I'm smiling" />
+          <img className="image-about" src={image01}/>
         </div>
         <p>
-          Lorem ipsum
+          Hello, my name is Dali, I'm a brazilian developer. I have been working in the technology industry since 2016, and during that time I had the experience of developing things in Javascript, mostly web and mobile applications.
+        </p>
+        <p>
+          I was born in the interior of Paraíba and then went to live in a place called Tacaratu, in the interior of Pernambuco, where I spent a lot of time studying and started to dream of creating my first codes and through them to build a life different from the one I knew. For me, technology was a gift because, thanks to it, I was able to access the great world repository of knowledge, the Internet, and through it I got to know a new world, where I could be who I was and still start to develop my skills to build a career.
+        </p>
+        <p>
+          I believe that knowledge is the most empowering thing that we can share in the community. Just as technology was a gift that changed my life, I believe it can make a difference in other people's lives, so I love creating connections to share knowledge.
+        </p>
+        <p>
+          I really like to share the things I learn, and I did it most of my time working as a teacher in an education program in advanced digital technologies that takes place in favelas in Rio de Janeiro called VaiNaWeb.
+        </p>
+        <p>
+          I also had the opportunity to make many significant exchanges of knowledge in some other code communities created to support underrepresented groups in technology, such as PyLadies, Django Girls and ReactLadies.
         </p>
         <div className="about-gallery">
-          <img className="image-about" src={image03} alt="I'm smiling" />
-          <img className="image-about" src={image04} alt="I'm smiling" />
+          <img className="image-about" src={image03}/>
+          <img className="image-about" src={image09}/>
         </div>
         <div className="about-gallery">
-          <img className="image-about" src={image08} alt="I'm smiling" />
-          <img className="image-about" src={image07} alt="I'm smiling" />
+          <img className="image-about" src={image08}/>
+          <img className="image-about" src={image07}/>
         </div>
         <div className="about-gallery">
-          <img className="image-about" src={image06} alt="I'm smiling" />
-          <img className="image-about" src={image05} alt="I'm smiling" />
+          <img className="image-about" src={image06}/>
+          <img className="image-about" src={image05}/>
         </div>
       </div>
     </Layout>
